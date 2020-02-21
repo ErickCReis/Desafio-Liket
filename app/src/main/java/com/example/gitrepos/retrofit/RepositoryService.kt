32aff16bc0e7c1b163e61e635daff1427311ba6e
ErickCReis <erickcorreareis@gmail.com>
2020-02-21 19:12:31 -0300

    Pegando imagem do usuário

--- app/src/main/java/com/example/gitrepos/retrofit/RepositoryService.kt
+++ app/src/main/java/com/example/gitrepos/retrofit/RepositoryService.kt
@@ -2,14 +2,16 @@ package com.example.gitrepos.retrofit
 
 import com.example.gitrepos.model.Owner
 import com.example.gitrepos.model.Repositories
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface RepositoryService {
     @GET("repositories?q=language:swift&sort=stars")
     fun getRepositories(): Call<Repositories>
 
-    @GET()
-    fun getAvatar(): Call<Owner>
+    @GET
+    fun getAvatar(@Url url: String): Call<ResponseBody>
 }
--- app/src/main/java/com/example/gitrepos/view/MainActivity.kt
+++ app/src/main/java/com/example/gitrepos/view/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.gitrepos.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.gitrepos.model.Repositories
 import com.example.gitrepos.R
 import com.example.gitrepos.model.Item
+import com.example.gitrepos.model.Repositories
 import com.example.gitrepos.retrofit.RepositoryService
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.ResponseBody
+import okhttp3.internal.http2.Http2Stream
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getData()
+        getAvatar("https://avatars2.githubusercontent.com/u/484656")
     }
 
     fun getData() {
@@ -37,6 +41,7 @@ class MainActivity : AppCompatActivity() {
 
                 for (item in items.listIterator()){
                     println(item.name)
+                    //getAvatar(item.owner.avatarUrl)
                 }
                 text.text = "Complete"
         }
@@ -45,6 +50,26 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+    }
+
+    fun getAvatar(url: String) {
+
+        val retrofitClient = Retrofit.Builder()
+            .baseUrl("http://example.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val endpoint = retrofitClient.create(RepositoryService::class.java)
+        val callback = endpoint.getAvatar(url)
+
+        callback.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                println(response.body()!!.byteStream())
+            }
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                text.text = t.message
+            }
+        })
     }
 }
 
