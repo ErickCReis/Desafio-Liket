d25061d6a4a68cfc385ad712351aac667c3195a9
ErickCReis <erickcorreareis@gmail.com>
2020-02-22 11:01:22 -0300

    Exibindo imagens dos usuários

--- app/src/main/java/com/example/gitrepos/retrofit/RepositoryService.kt
+++ app/src/main/java/com/example/gitrepos/retrofit/RepositoryService.kt
@@ -1,16 +1,14 @@
 package com.example.gitrepos.retrofit
 
-import com.example.gitrepos.model.Owner
 import com.example.gitrepos.model.Repositories
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Url
 
 interface RepositoryService {
-    @GET("repositories?q=language:swift&sort=stars")
-    fun getRepositories(): Call<Repositories>
+    @GET
+    fun getRepositories(@Url url: String): Call<Repositories>
 
     @GET
     fun getAvatar(@Url url: String): Call<ResponseBody>
--- app/src/main/java/com/example/gitrepos/view/MainActivity.kt
+++ app/src/main/java/com/example/gitrepos/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gitrepos.view
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gitrepos.R
@@ -8,7 +9,6 @@ import com.example.gitrepos.model.Repositories
 import com.example.gitrepos.retrofit.RepositoryService
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.ResponseBody
-import okhttp3.internal.http2.Http2Stream
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,17 +23,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getData()
-        getAvatar("https://avatars2.githubusercontent.com/u/484656")
     }
 
-    fun getData() {
+    private fun getData() {
         val retrofitClient = Retrofit.Builder()
-            .baseUrl("https://api.github.com/search/")
+            .baseUrl("http://example.com/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         val endpoint = retrofitClient.create(RepositoryService::class.java)
-        val callback = endpoint.getRepositories()
+        val callback = endpoint.getRepositories("https://api.github.com/search/repositories?q=language:swift&sort=stars")
 
         callback.enqueue(object : Callback<Repositories> {
             override fun onResponse(call: Call<Repositories>, response: Response<Repositories>) {
@@ -41,7 +40,7 @@ class MainActivity : AppCompatActivity() {
 
                 for (item in items.listIterator()){
                     println(item.name)
-                    //getAvatar(item.owner.avatarUrl)
+                    getAvatar(item.owner.avatarUrl)
                 }
                 text.text = "Complete"
         }
@@ -49,10 +48,9 @@ class MainActivity : AppCompatActivity() {
                 text.text = t.message
             }
         })
-
     }
 
-    fun getAvatar(url: String) {
+    private fun getAvatar(url: String) {
 
         val retrofitClient = Retrofit.Builder()
             .baseUrl("http://example.com/api/")
@@ -64,7 +62,8 @@ class MainActivity : AppCompatActivity() {
 
         callback.enqueue(object : Callback<ResponseBody> {
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                println(response.body()!!.byteStream())
+                val image = response.body()!!.byteStream()
+                imageView.setImageBitmap(BitmapFactory.decodeStream(image))
             }
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                 text.text = t.message
